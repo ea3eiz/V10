98def0e4a46e86b9c1d063fe0c33b73bdc0e51e1
ea3eiz <ea3eiz@ea3eiz.com>
2019-07-14 23:13:16 +0200

    Update ejecutar_NXDN.sh

--- ejecutar_NXDN.sh
+++ ejecutar_NXDN.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-#mode=`grep -n -m 1 "^Port=" /home/pi/MMDVMHost/MMDVMDMR2NXDN.ini`
-#buscar=":"
-#caracteres=`expr index $mode $buscar`
-#caracteres_linea=`expr $caracteres - 1`
-#numero_linea_port=`expr substr $mode 1 $caracteres_linea`
-#mode=$(awk "NR==$numero_linea_port" /home/pi/MMDVMHost/MMDVMDMR2NXDN.ini)
-#puerto=`expr substr $mode 14 9`
-#puerto="  "$puerto
-#cd /home/pi/Desktop
-#sudo cp RXF_DMR2NXDN.desktop /home/pi
-#frecuencia=$(awk "NR==13" /home/pi/MMDVMHost/MMDVMDMR2NXDN.ini)
-#frecuencia=`expr substr $frecuencia 13 9`
-#frecuencia=$frecuencia$puerto
-#sed -i "11c Name=$frecuencia" /home/pi/RXF_DMR2NXDN.desktop
-#sleep 1
-#sudo cp /home/pi/RXF_DMR2NXDN.desktop /home/pi/Desktop
-#sleep 1
-#sudo rm /home/pi/RXF_DMR2NXDN.desktop
+mode=`grep -n -m 1 "^Port=" /home/pi/MMDVMHost/MMDVMNXDN.ini`
+buscar=":"
+caracteres=`expr index $mode $buscar`
+caracteres_linea=`expr $caracteres - 1`
+numero_linea_port=`expr substr $mode 1 $caracteres_linea`
+mode=$(awk "NR==$numero_linea_port" /home/pi/MMDVMHost/MMDVMNXDN.ini)
+puerto=`expr substr $mode 14 9`
+puerto="  "$puerto
+cd /home/pi/Desktop
+sudo cp RXF_NXDN.desktop /home/pi
+frecuencia=$(awk "NR==13" /home/pi/MMDVMHost/MMDVMNXDN.ini)
+frecuencia=`expr substr $frecuencia 13 9`
+frecuencia=$frecuencia$puerto
+sed -i "11c Name=$frecuencia" /home/pi/RXF_NXDN.desktop
+sleep 1
+sudo cp /home/pi/RXF_NXDN.desktop /home/pi/Desktop
+sleep 1
+sudo rm /home/pi/RXF_NXDN.desktop
 
 SCRIPTS_version=$(awk "NR==1" /home/pi/.config/autostart/version)
 cd /home/pi/Desktop
