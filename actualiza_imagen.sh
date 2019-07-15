b90af4664663ac88f6883d0caeb8e5e742deae72
ea3eiz <ea3eiz@ea3eiz.com>
2019-07-15 18:39:51 +0200

    1

--- actualiza_imagen.sh
+++ actualiza_imagen.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 SCRIPTS_version=$(awk "NR==1" /home/pi/.config/autostart/version)
-version=`expr substr $SCRIPTS_version2 2`
+version=`expr substr $SCRIPTS_version 2 2`
 AUTOARRANQUE="AUTOARRANQUE"
 AUTOARRANQUE=$AUTOARRANQUE$SCRIPTS_version
 
