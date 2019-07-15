eaeaf627f5bf3f7e24316baf04aa25d781a836b2
ea3eiz <ea3eiz@ea3eiz.com>
2019-07-16 00:05:07 +0200

    1

--- ejecutar_bm.sh
+++ ejecutar_bm.sh
@@ -18,6 +18,8 @@ sudo cp /home/pi/RXF_BM.desktop /home/pi/Desktop
 sleep 1
 sudo rm /home/pi/RXF_BM.desktop
 
+sed -i "1c $frecuencia" /home/pi/INFO_RXF
+
 SCRIPTS_version=$(awk "NR==1" /home/pi/.config/autostart/version)
 cd /home/pi/Desktop
 sudo cp Abrir_MMDVMBM.desktop /home/pi
