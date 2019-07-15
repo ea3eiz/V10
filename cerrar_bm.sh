835354dcb521045640f79bfd08dc6cff6d608d28
ea3eiz <ea3eiz@ea3eiz.com>
2019-07-15 20:03:40 +0200

    1

--- cerrar_bm.sh
+++ cerrar_bm.sh
@@ -4,7 +4,7 @@ cd /home/pi/Desktop
 sudo cp Abrir_MMDVMBM.desktop /home/pi
 sleep 1
 sed -i "4cExec=sh -c 'cd /home/pi/$SCRIPTS_version; lxterminal --geometry=72x15 -e sudo sh ejecutar_bm.sh'" /home/pi/Abrir_MMDVMBM.desktop
-sed -i "5c Icon=/home/pi/$SCRIPTS_version/DMR.png" /home/pi/Abrir_MMDVMBM.desktop
+sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICONO_BM_OFF.png" /home/pi/Abrir_MMDVMBM.desktop
 sed -i "10c Name[es_ES]=Abrir BM" /home/pi/Abrir_MMDVMBM.desktop
 sed -i "7c MMDVMBM=OFF" /home/pi/status.ini
 cd /home/pi
