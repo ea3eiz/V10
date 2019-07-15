192b39967354562b392d8ce7ed89bdf2b37ab0bb
ea3eiz <ea3eiz@ea3eiz.com>
2019-07-15 23:06:05 +0200

    1

--- ejecutar_NXDN.sh
+++ ejecutar_NXDN.sh
@@ -22,7 +22,7 @@ SCRIPTS_version=$(awk "NR==1" /home/pi/.config/autostart/version)
 cd /home/pi/Desktop
 sudo cp Abrir_NXDN.desktop /home/pi
 sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version;lxterminal --geometry=72x15 -e sudo sh cerrar_NXDN.sh'" /home/pi/Abrir_NXDN.desktop
-sed -i "5c Icon=/home/pi/$SCRIPTS_version/DMR2NXDN_ON.png" /home/pi/Abrir_NXDN.desktop
+sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICONO_NXDN_ON.png" /home/pi/Abrir_NXDN.desktop
 sed -i "10c Name[es_ES]=Cerrar NXDN" /home/pi/Abrir_NXDN.desktop
 sed -i "17c NXDN=ON" /home/pi/status.ini
 cd /home/pi
