534212e72d724c25832bceffb4d65fdde8545b48
ea3eiz <ea3eiz@ea3eiz.com>
2019-07-17 22:14:34 +0200

    1

---
+++ ejecutar_dvrptr.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+SCRIPTS_version=$(awk "NR==1" /home/pi/.config/autostart/version)
+cd /home/pi/Desktop
+cp Boton_3 /home/pi
+sed -i "6c Exec=sh -c 'cd /home/pi/$SCRIPTS_version; sudo sh cerrar_dvrptr.sh'" /home/pi/Abrir_Dvrptr.desktop
+sed -i "7c Icon=/home/pi/$SCRIPTS_version/DVRPTR_ON.png" /home/pi/Abrir_Dvrptr.desktop
+sed -i "11c Name[es_ES]=cerrar Dvrptr" /home/pi/Abrir_Dvrptr.desktop
+cd /home/pi
+cp Abrir_Dvrptr.desktop /home/pi/Desktop
+sudo rm /home/pi/Abrir_Dvrptr.desktop
+cd /home/pi/dvrptr
+mono dvrptr.exe
+ 
+
