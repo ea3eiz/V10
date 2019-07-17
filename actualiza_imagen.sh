08b6ef1f138577108a0a04fe226a4569757fa69a
ea3eiz <ea3eiz@ea3eiz.com>
2019-07-17 23:18:52 +0200

    1

--- actualiza_imagen.sh
+++ actualiza_imagen.sh
@@ -42,7 +42,8 @@ sleep 2
 #Lee el fichero INFO_NXDN para poner lod datos en los iconos INFO TXF                        
 frecuencia=$(awk "NR==1" /home/pi/INFO_RXF)
 sed -i "11c Name=$frecuencia" /home/pi/Desktop/RXF_BM.desktop
-
+frecuencia=$(awk "NR==2" /home/pi/INFO_RXF)
+sed -i "11c Name=$frecuencia" /home/pi/Desktop/RXF_DMRPLUS.desktop
 frecuencia=$(awk "NR==10" /home/pi/INFO_RXF)
 sed -i "11c Name=$frecuencia" /home/pi/Desktop/RXF_NXDN.desktop
 
