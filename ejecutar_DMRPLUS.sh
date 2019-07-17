578bbe5ca8d5bf198ba7f5ea3eaf646f500efacb
ea3eiz <ea3eiz@ea3eiz.com>
2019-07-17 23:05:22 +0200

    1

--- ejecutar_DMRPLUS.sh
+++ ejecutar_DMRPLUS.sh
@@ -20,15 +20,15 @@ sudo rm /home/pi/RXF_DMRPLUS.desktop
 
 SCRIPTS_version=$(awk "NR==1" /home/pi/.config/autostart/version)
 cd /home/pi/Desktop
-sudo cp Abrir_MMDVMPLUS /home/pi
-sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version;lxterminal --geometry=72x15 -e sudo sh cerrar_plus_30.sh'" /home/pi/Abrir_MMDVMPLUS
-sed -i "5c Icon=/home/pi/$SCRIPTS_version/DMRPLUS_ON.png" /home/pi/Abrir_MMDVMPLUS
-sed -i "10c Name[es_ES]=Cerrar DMR+" /home/pi/Abrir_MMDVMPLUS
+sudo cp Abrir_MMDVMPLUS.desktop /home/pi
+sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version;lxterminal --geometry=72x15 -e sudo sh cerrar_DMRPLUS.sh'" /home/pi/Abrir_MMDVMPLUS.desktop
+sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICONO_DMRPLUS_ON.png" /home/pi/Abrir_MMDVMPLUS.desktop
+sed -i "10c Name[es_ES]=Cerrar DMR+" /home/pi/Abrir_MMDVMPLUS.desktop
 sed -i "6c MMDVMPLUS=ON" /home/pi/status.ini
 cd /home/pi
-sudo cp Abrir_MMDVMPLUS /home/pi/Desktop
+sudo cp Abrir_MMDVMPLUS.desktop /home/pi/Desktop
 sleep 1
-sudo rm /home/pi/Abrir_MMDVMPLUS
+sudo rm /home/pi/Abrir_MMDVMPLUS.desktop
 
 cd /home/pi/MMDVMHost
 echo "\33[1;37m"
@@ -39,11 +39,11 @@ sleep 1
 sudo ./MMDVMPLUS MMDVMPLUS.ini
 
 #cd /home/pi/Desktop
-#sudo cp Abrir_MMDVMPLUS /home/pi
-#sed -i "4cExec=sh -c 'cd /home/pi/$SCRIPTS_version;lxterminal --geometry=72x15 -e sudo sh ejecutar_plus_30.sh'" /home/pi/Abrir_MMDVMPLUS
-#sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICONO_DMRPLUS.png" /home/pi/Abrir_MMDVMPLUS
-#sed -i "10c Name[es_ES]=Abrir DMR+" /home/pi/Abrir_MMDVMPLUS
+#sudo cp Abrir_MMDVMPLUS.desktop /home/pi
+#sed -i "4cExec=sh -c 'cd /home/pi/$SCRIPTS_version;lxterminal --geometry=72x15 -e sudo sh ejecutar_plus_30.sh'" /home/pi/Abrir_MMDVMPLUS.desktop
+#sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICONO_DMRPLUS.png" /home/pi/Abrir_MMDVMPLUS.desktop
+#sed -i "10c Name[es_ES]=Abrir DMR+" /home/pi/Abrir_MMDVMPLUS.desktop
 #sed -i "6c MMDVMPLUS=OFF" /home/pi/status.inicd /home/pi
-#sudo cp Abrir_MMDVMPLUS /home/pi/Desktop
+#sudo cp Abrir_MMDVMPLUS.desktop /home/pi/Desktop
 #sleep 1
-#sudo rm /home/pi/Abrir_MMDVMPLUS
+#sudo rm /home/pi/Abrir_MMDVMPLUS.desktop
