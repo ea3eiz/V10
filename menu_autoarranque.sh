f344c69c86b9bae317cc93042202f318144f9283
ea3eiz <ea3eiz@ea3eiz.com>
2019-07-17 18:20:50 +0200

    Update menu_autoarranque.sh

--- menu_autoarranque.sh
+++ menu_autoarranque.sh
@@ -454,7 +454,18 @@ fi
 trans=`grep "DMR2NXDN" /home/pi/autoarranque.ini`
 #==================================================================================
 
-
+#==================================================================================
+echo -n "\33[1;36m   217)\33[1;33m  Poner NXDN en el autoarranque     - "
+var1=$(awk "NR==18" /home/pi/autoarranque.ini)
+var1=`expr substr $var1 6 3`
+if [ $var1 = "OFF" ]
+then
+echo "\33[1;31m$var1"
+else
+echo "\33[1;31m"
+fi
+trans=`grep "NXDN" /home/pi/autoarranque.ini`
+#==================================================================================
 
 
 echo "\33[0m "
