681fe4d85dd92c22bf3b0aaab745a038d3c08fb1
ea3eiz <ea3eiz@ea3eiz.com>
2019-07-20 17:48:53 +0200

    1

--- ejecutar_bluedv.sh
+++ ejecutar_bluedv.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 SCRIPTS_version=$(awk "NR==1" /home/pi/.config/autostart/version)
 cd /home/pi/Desktop
-sudo cp Abrir_BlueDV /home/pi
-sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version; sudo sh cerrar_bluedv.sh'" /home/pi/Abrir_BlueDV
-sed -i "5c Icon=/home/pi/$SCRIPTS_version/BLUEDV_ON.png" /home/pi/Abrir_BlueDV
-sed -i "10c Name[es_ES]=Cerrar BlueDV" /home/pi/Abrir_BlueDV
+sudo cp Abrir_BlueDV.desktop /home/pi
+sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version; sudo sh cerrar_bluedv.sh'" /home/pi/Abrir_BlueDV.desktop
+sed -i "5c Icon=/home/pi/$SCRIPTS_version/BLUEDV_ON.png" /home/pi/Abrir_BlueDV.desktop
+sed -i "10c Name[es_ES]=Cerrar BlueDV" /home/pi/Abrir_BlueDV.desktop
 sed -i "2c BlueDV=ON" /home/pi/status.ini
 cd /home/pi
-sudo cp Abrir_BlueDV /home/pi/Desktop
+sudo cp Abrir_BlueDV.desktop /home/pi/Desktop
 sleep 1
-sudo rm /home/pi/Abrir_BlueDV
+sudo rm /home/pi/Abrir_BlueDV.desktop
 
 cd /home/pi/bluedv/
 mono BlueDV.exe
