f99eb9042caf8602bbc57866f7c73b9f20e7a202
Manuel Díaz Jiménez <manel@iMac-de-Manuel.local>
2019-08-08 20:12:46 +0200

    1

--- ejecutar_YSF2DMR.sh
+++ ejecutar_YSF2DMR.sh
@@ -22,7 +22,7 @@ SCRIPTS_version=$(awk "NR==1" /home/pi/.config/autostart/version)
 cd /home/pi/Desktop
 sudo cp Abrir_YSF2DMR.desktop /home/pi
 sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version/;lxterminal --geometry=80x20 -e sudo sh cerrar_YSF2DMR.sh'" /home/pi/Abrir_YSF2DMR.desktop
-sed -i "5c Icon=/home/pi/$SCRIPTS_version/YSF2DMR_ON.png" /home/pi/Abrir_YSF2DMR.desktop
+sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICONO_YSF2DMR_ON.png" /home/pi/Abrir_YSF2DMR.desktop
 sed -i "10c Name[es_ES]=Cerrar YSF2DMR" /home/pi/Abrir_YSF2DMR.desktop
 sed -i "14c YSF2DMR=ON" /home/pi/status.ini
 cd /home/pi
