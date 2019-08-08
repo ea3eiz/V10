3ebbe19b17847d8cd056ed5c08b4fcec6c56dab1
Manuel Díaz Jiménez <manel@iMac-de-Manuel.local>
2019-08-08 19:16:53 +0200

    1

--- ejecutar_DMR2NXDN.sh
+++ ejecutar_DMR2NXDN.sh
@@ -22,7 +22,7 @@ SCRIPTS_version=$(awk "NR==1" /home/pi/.config/autostart/version)
 cd /home/pi/Desktop
 sudo cp Abrir_DMR2NXDN.desktop /home/pi
 sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version;lxterminal --geometry=72x15 -e sudo sh cerrar_DMR2NXDN.sh'" /home/pi/Abrir_DMR2NXDN.desktop
-sed -i "5c Icon=/home/pi/$SCRIPTS_version/DMR2NXDN_ON.png" /home/pi/Abrir_DMR2NXDN.desktop
+sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICONO_DMR2NXDN_ON.png" /home/pi/Abrir_DMR2NXDN.desktop
 sed -i "10c Name[es_ES]=Cerrar DMR2NXDN" /home/pi/Abrir_DMR2NXDN.desktop
 sed -i "16c DMR2NXDN=ON" /home/pi/status.ini
 cd /home/pi
