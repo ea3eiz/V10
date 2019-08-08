868155ee6b0eed55ba96298e9dabdc8eb913689c
Manuel Díaz Jiménez <manel@iMac-de-Manuel.local>
2019-08-08 18:08:46 +0200

    Update ejecutar_ambe_server.sh

--- ejecutar_ambe_server.sh
+++ ejecutar_ambe_server.sh
@@ -3,7 +3,7 @@ SCRIPTS_version=$(awk "NR==1" /home/pi/.config/autostart/version)
 cd /home/pi/Desktop
 sudo cp abrir_ambe_server.desktop /home/pi
 sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version;lxterminal --geometry=72x15 -e sudo sh cerrar_ambe_server.sh'" /home/pi/abrir_ambe_server.desktop
-sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICONO_AMBER_SERVER_ON.png" /home/pi/abrir_ambe_server.desktop
+sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICONO_AMBE_SERVER_ON.png" /home/pi/abrir_ambe_server.desktop
 sed -i "10c Name[es_ES]=Cerrar AMBE SERVER" /home/pi/abrir_ambe_server.desktop
 sed -i "11c AMBE_SERVER=ON" /home/pi/status.ini
 
