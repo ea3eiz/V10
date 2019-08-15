65d3a7592d6b8c0b546157c9f245ebb44ad9c211
Manuel Díaz Jiménez <manel@iMac-de-Manuel.local>
2019-08-16 01:13:27 +0200

    1

--- cerrar_dv4mini.sh
+++ cerrar_dv4mini.sh
@@ -4,14 +4,14 @@ sudo killall dv_serial
 sudo killall python
 SCRIPTS_version=$(awk "NR==1" /home/pi/.config/autostart/version)
 cd /home/pi/Desktop
-sudo cp Abrir_dv4mini /home/pi
+sudo cp Abrir_dv4mini.desktop /home/pi
 sleep 1
-sed -i "4cExec=sh -c 'cd /home/pi/$SCRIPTS_version; sudo sh ejecutar_dv4mini.sh'" /home/pi/Abrir_dv4mini
-sed -i "5c Icon=/home/pi/$SCRIPTS_version/dv4k.png" /home/pi/Abrir_dv4mini
-sed -i "10c Name[es_ES]=Abrir DV4mini" /home/pi/Abrir_dv4mini
-sed -i "4c DV4mini=OFF" /home/pi/status.ini
+sed -i "4cExec=sh -c 'cd /home/pi/$SCRIPTS_version; sudo sh ejecutar_dv4mini.sh'" /home/pi/Abrir_dv4mini.desktop
+sed -i "5c Icon=/home/pi/$SCRIPTS_version/dv4k.png" /home/pi/Abrir_dv4mini.desktop
+sed -i "10c Name[es_ES]=Abrir DV4mini" /home/pi/Abrir_dv4mini.desktop
+sed -i "4c DV4mini=OFF" /home/pi/status.ini.desktop
 sleep 1
 cd /home/pi
-sudo cp Abrir_dv4mini /home/pi/Desktop
+sudo cp Abrir_dv4mini.desktop /home/pi/Desktop
 sleep 1
-sudo rm /home/pi/Abrir_dv4mini
+sudo rm /home/pi/Abrir_dv4mini.desktop
