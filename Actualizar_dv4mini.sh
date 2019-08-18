93107a4b76d744f277cc4c38c9f5a5af90690a1e
Manuel Díaz Jiménez <manel@iMac-de-Manuel.local>
2019-08-18 09:37:43 +0200

    Update Actualizar_dv4mini.sh

--- Actualizar_dv4mini.sh
+++ Actualizar_dv4mini.sh
@@ -1,9 +1,18 @@
 ﻿#!/bin/bash
+clear
 while true
 do
-SCRIPTS_version=$(awk "NR==1" /home/pi/.config/autostart/version)        
-clear
-echo "\33[1;32m   *********************************************************************"
+SCRIPTS_version=$(awk "NR==1" /home/pi/.config/autostart/version)
+#Colores
+ROJO="\033[1;31m"
+VERDE="\033[1;32m"
+BLANCO="\033[1;37m"
+AMARILLO="\033[1;33m"
+CIAN="\033[1;36m"
+GRIS="\033[0m"
+
+echo "${VERDE}"         
+echo "   *********************************************************************"
 echo "   *                                                                   *"
 echo "   *           Script para Actualizar \33[1;33mDV4MINI20190307   \33[1;31m by EA3EIZ\33[1;32m     *"
 echo "   *                                                                   *"
@@ -11,10 +20,9 @@ echo "   *********************************************************************"
 echo  "\33[1;36m   1)\33[1;33m Actualizar \33[1;32mDV4MINI20190307   \33[1;33m"
 echo  "\33[1;36m   2)\33[1;33m Volver a la versión \33[1;32mDV4MINI20190127   \33[1;33m"
 echo  "\33[1;36m   3)\33[1;33m Volver a la versión \33[1;32mDV4MINI20170517   \33[1;33m"
+echo  "\33[1;36m   4)\33[1;33m Actualizar \33[1;32mDV4MINI20190307${AMARILLO} (NEW)   \33[1;33m"
 echo ""
-echo "\33[1;36m   0)\33[1;34m Salir del script \33[1;31m OJO!! no salir con ctrl+c ni con la x"
-echo ""
-echo -n "\33[1;36m   Elige una opción del 0 al 2: " 
+echo -n "\33[1;36m   Elige una opción: " 
 read escoger_menu
 
 case $escoger_menu in
@@ -30,7 +38,8 @@ clear
                         sudo rm -r /home/pi/$SCRIPTS_version/DV4MINI/
                         cd /home/pi/$SCRIPTS_version
                         git clone http://github.com/ea3eiz/DV4MINI
-                        sudo rm /home/pi/dv4mini/*.*
+                        sudo rm -r /home/pi/dv4mini
+                        sudo rm /usr/bin/dv_serial
                         cd /home/pi/$SCRIPTS_version/DV4MINI/20190307
                         cp dv_serial /home/pi/dv4mini
                         cp dv4mini.exe /home/pi/dv4mini
@@ -64,6 +73,7 @@ clear
                         cd /home/pi/$SCRIPTS_version
                         git clone http://github.com/ea3eiz/DV4MINI
                         sudo rm -r /home/pi/dv4mini
+                        sudo rm /usr/bin/dv_serial
                         sudo mkdir /home/pi/dv4mini
                         cd /home/pi/$SCRIPTS_version/DV4MINI/20190127
                         cp dv_serial /home/pi/dv4mini
@@ -98,6 +108,7 @@ clear
                         cd /home/pi/$SCRIPTS_version
                         git clone http://github.com/ea3eiz/DV4MINI
                         sudo rm -r /home/pi/dv4mini
+                        sudo rm /usr/bin/dv_serial
                         sudo mkdir /home/pi/dv4mini
                         cd /home/pi/$SCRIPTS_version/DV4MINI/20170517
                         cp dv_serial /home/pi/dv4mini
@@ -121,7 +132,7 @@ esac
 done;;
 
 
-1000) echo ""
+4) echo ""
 while true
 do
 clear
@@ -134,6 +145,7 @@ clear
                         cd /home/pi/$SCRIPTS_version
                         git clone http://github.com/ea3eiz/DV4MINI
                         sudo rm -r /home/pi/dv4mini
+                        sudo rm /usr/bin/dv_serial
                         sudo mkdir /home/pi/dv4mini
                         cd /home/pi/$SCRIPTS_version/DV4MINI/20190307new
                         cp dv_serial /home/pi/dv4mini
