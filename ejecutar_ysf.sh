e383e12d2d54ddd2dc3e822df84532fb8b51e723
Manuel Díaz Jiménez <manel@iMac-de-Manuel.local>
2019-09-11 00:07:36 +0200

    Update ejecutar_ysf.sh

--- ejecutar_ysf.sh
+++ ejecutar_ysf.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 SCRIPTS_version=$(awk "NR==1" /home/pi/.config/autostart/version)
 cd /home/pi/Desktop
-sudo cp YSFGateway /home/pi
-sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version;lxterminal --geometry=72x15 -e sh cerrar_ysf.sh'" /home/pi/YSFGateway
-sed -i "5c Icon=/home/pi/$SCRIPTS_version/YSF_ON.png" /home/pi/YSFGateway
-sed -i "10c Name[es_ES]=Cerrar YSF" /home/pi/YSFGateway
+sudo cp Abrir_YSF.desktop /home/pi
+sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version;lxterminal --geometry=72x15 -e sh cerrar_ysf.sh'" /home/pi/Abrir_YSF.desktop
+sed -i "5c Icon=/home/pi/$SCRIPTS_version/YSF_ON.png" /home/pi/Abrir_YSF.desktop
+sed -i "10c Name[es_ES]=Cerrar YSF" /home/pi/Abrir_YSF.desktop
 sed -i "3c YSF=ON" /home/pi/status.ini
 cd /home/pi
-sudo cp YSFGateway /home/pi/Desktop
+sudo cp Abrir_YSF.desktop /home/pi/Desktop
 sleep 1
-sudo rm /home/pi/YSFGateway
+sudo rm /home/pi/Abrir_YSF.desktop
 
-cd /home/pi/YSFClients/YSFGateway
+cd /home/pi/YSFClients/Abrir_YSF.desktop
 echo "\33[1;32m"
 clear
 
