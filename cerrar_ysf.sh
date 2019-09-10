268f61adfeafb070cea0af28e902d0d569a13b81
Manuel Díaz Jiménez <manel@iMac-de-Manuel.local>
2019-09-11 00:09:48 +0200

    Update cerrar_ysf.sh

--- cerrar_ysf.sh
+++ cerrar_ysf.sh
@@ -2,14 +2,14 @@
 sudo killall YSFGateway
 SCRIPTS_version=$(awk "NR==1" /home/pi/.config/autostart/version)
 cd /home/pi/Desktop
-sudo cp YSFGateway /home/pi
+sudo cp Abrir_YSF.desktop /home/pi
 sleep 1
-sed -i "4cExec=sh -c 'cd /home/pi/$SCRIPTS_version;lxterminal --geometry=72x15 -e sh ejecutar_ysf.sh'" /home/pi/YSFGateway
-sed -i "5c Icon=/home/pi/$SCRIPTS_version/C4FM.png" /home/pi/YSFGateway
-sed -i "10c Name[es_ES]=Abrir YSF" /home/pi/YSFGateway
+sed -i "4cExec=sh -c 'cd /home/pi/$SCRIPTS_version;lxterminal --geometry=72x15 -e sh ejecutar_ysf.sh'" /home/pi/Abrir_YSF.desktop
+sed -i "5c Icon=/home/pi/$SCRIPTS_version/C4FM.png" /home/pi/Abrir_YSF.desktop
+sed -i "10c Name[es_ES]=Abrir YSF" /home/pi/Abrir_YSF.desktop
 sed -i "3c YSF=OFF" /home/pi/status.ini
 cd /home/pi
 sleep 1
-sudo cp YSFGateway /home/pi/Desktop
+sudo cp Abrir_YSF.desktop /home/pi/Desktop
 sleep 1
-sudo rm /home/pi/YSFGateway
+sudo rm /home/pi/Abrir_YSF.desktop
