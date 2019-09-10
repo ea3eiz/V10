62f968afb31bb33ef3e68163ce3f5020e281795a
Manuel Díaz Jiménez <manel@iMac-de-Manuel.local>
2019-09-11 01:21:22 +0200

    1

--- ejecutar_ysf.sh
+++ ejecutar_ysf.sh
@@ -2,7 +2,7 @@
 SCRIPTS_version=$(awk "NR==1" /home/pi/.config/autostart/version)
 cd /home/pi/Desktop
 sudo cp Abrir_YSF.desktop /home/pi
-sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version;lxterminal --geometry=72x15 -e sh cerrar_ysf.sh'" /home/pi/Abrir_YSF.desktop
+sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version;lxterminal --geometry=72x15 -e sudo sh cerrar_ysf.sh'" /home/pi/Abrir_YSF.desktop
 sed -i "5c Icon=/home/pi/$SCRIPTS_version/YSF_ON.png" /home/pi/Abrir_YSF.desktop
 sed -i "10c Name[es_ES]=Cerrar YSF" /home/pi/Abrir_YSF.desktop
 sed -i "3c YSF=ON" /home/pi/status.ini
