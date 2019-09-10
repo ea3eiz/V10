bf9fc78316c09798594fd01f95f1190bb8922b0d
Manuel Díaz Jiménez <manel@iMac-de-Manuel.local>
2019-09-11 00:00:27 +0200

    1

--- cerrar_ysf.sh
+++ cerrar_ysf.sh
@@ -4,7 +4,7 @@ SCRIPTS_version=$(awk "NR==1" /home/pi/.config/autostart/version)
 cd /home/pi/Desktop
 sudo cp YSFGateway /home/pi
 sleep 1
-sed -i "4cExec=sh -c 'cd /home/pi/$SCRIPTS_version;lxterminal --geometry=72x15 -e sudo sh ejecutar_ysf_30.sh'" /home/pi/YSFGateway
+sed -i "4cExec=sh -c 'cd /home/pi/$SCRIPTS_version;lxterminal --geometry=72x15 -e sh ejecutar_ysf.sh'" /home/pi/YSFGateway
 sed -i "5c Icon=/home/pi/$SCRIPTS_version/C4FM.png" /home/pi/YSFGateway
 sed -i "10c Name[es_ES]=Abrir YSF" /home/pi/YSFGateway
 sed -i "3c YSF=OFF" /home/pi/status.ini
--- ejecutar_ysf.sh
+++ ejecutar_ysf.sh
@@ -2,7 +2,7 @@
 SCRIPTS_version=$(awk "NR==1" /home/pi/.config/autostart/version)
 cd /home/pi/Desktop
 sudo cp YSFGateway /home/pi
-sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version;lxterminal --geometry=72x15 -e sudo sh cerrar_ysf_30.sh'" /home/pi/YSFGateway
+sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version;lxterminal --geometry=72x15 -e sh cerrar_ysf.sh'" /home/pi/YSFGateway
 sed -i "5c Icon=/home/pi/$SCRIPTS_version/YSF_ON.png" /home/pi/YSFGateway
 sed -i "10c Name[es_ES]=Cerrar YSF" /home/pi/YSFGateway
 sed -i "3c YSF=ON" /home/pi/status.ini
