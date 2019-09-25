a5cd31e5f43d48522d7994d04f3ad1ca15a4da81
Manuel Díaz Jiménez <manel@iMac-de-Manuel.local>
2019-09-25 09:00:03 +0200

    Update md380tools.sh

--- md380tools.sh
+++ md380tools.sh
@@ -183,9 +183,9 @@ clear
                         echo "Introduciendo USER.BIN"
                         #sudo wget http://42561.noip.us/user.bin
                         #sudo wget https://kg5rki.com/MD380_AIO/user.bin
-                        #sudo wget --no-check-certificate https://radioid.net/static/user.csv
-                        #sudo mv user.csv user.bin
-                        #sleep 10
+                        sudo wget --no-check-certificate https://radioid.net/static/user.csv
+                        sudo mv user.csv user.bin
+                        sleep 10
                         sudo ./md380-tool spiflashwrite user.bin 0x1638400
                         break;;
                         [nN]* ) echo ""
