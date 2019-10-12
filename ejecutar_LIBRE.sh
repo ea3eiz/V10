276f9a40e2b768847d8e8bd7683f84bc063274d6
Manuel Díaz Jiménez <manel@iMac-de-Manuel.local>
2019-10-12 13:04:33 +0200

    1

--- ejecutar_LIBRE.sh
+++ ejecutar_LIBRE.sh
@@ -20,15 +20,15 @@ sudo rm /home/pi/RXF_LIBRE.desktop
 
 SCRIPTS_version=$(awk "NR==1" /home/pi/.config/autostart/version)
 cd /home/pi/Desktop
-sudo cp Abrir_LIBRE /home/pi
-sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version;lxterminal --geometry=72x15 -e sudo sh cerrar_LIBRE.sh'" /home/pi/Abrir_LIBRE
-sed -i "5c Icon=/home/pi/$SCRIPTS_version/LIBRE_ON.png" /home/pi/Abrir_LIBRE
-sed -i "10c Name[es_ES]=Cerrar LIBRE" /home/pi/Abrir_LIBRE
+sudo cp Abrir_LIBRE.desktop /home/pi
+sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version;lxterminal --geometry=72x15 -e sudo sh cerrar_LIBRE.sh'" /home/pi/Abrir_LIBRE.desktop
+sed -i "5c Icon=/home/pi/$SCRIPTS_version/LIBRE_ON.png" /home/pi/Abrir_LIBRE.desktop
+sed -i "10c Name[es_ES]=Cerrar LIBRE" /home/pi/Abrir_LIBRE.desktop
 sed -i "10c MMDVMLIBRE=ON" /home/pi/status.ini
 cd /home/pi
-sudo cp Abrir_LIBRE /home/pi/Desktop
+sudo cp Abrir_LIBRE.desktop /home/pi/Desktop
 sleep 1
-sudo rm /home/pi/Abrir_LIBRE
+sudo rm /home/pi/Abrir_LIBRE.desktop
 
 cd /home/pi/MMDVMHost
 clear
