15709e2e6e50b5db05d80492e07e64108d861e85
Manuel Díaz Jiménez <manel@iMac-de-Manuel.local>
2019-10-12 18:23:27 +0200

    1

--- cerrar_d-star.sh
+++ cerrar_d-star.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
-#cierra ircDDBGateway
-sudo killall ircddbgateway
+#cierra Abrir_ircDDBGateway.desktop
+sudo killall Abrir_ircDDBGateway.desktop
 #cierra remotecontrol
 sudo killall remotecontrol
 #cierra D-STARRepeater
 sudo killall dstarrepeater
 SCRIPTS_version=$(awk "NR==1" /home/pi/.config/autostart/version)
-#cierra ircDDBGateway y quita icono verde de "cerrar ircDDB"
+#cierra Abrir_ircDDBGateway.desktop y quita icono verde de "cerrar ircDDB"
 cd /home/pi/Desktop
-sudo cp ircDDBGateway /home/pi
+sudo cp Abrir_ircDDBGateway.desktop /home/pi
 sleep 1
-sed -i "4cExec=sh -c 'cd /home/pi/$SCRIPTS_version; sudo sh ejecutar_d-star_30.sh'" /home/pi/ircDDBGateway
-sed -i "5c Icon=/home/pi/$SCRIPTS_version/DSTAR.png" /home/pi/ircDDBGateway
-sed -i "10c Name[es_ES]=Abrir ircDDB" /home/pi/ircDDBGateway
+sed -i "4cExec=sh -c 'cd /home/pi/$SCRIPTS_version; sudo sh ejecutar_d-star.sh'" /home/pi/Abrir_ircDDBGateway.desktop
+sed -i "5c Icon=/home/pi/$SCRIPTS_version/DSTAR.png" /home/pi/Abrir_ircDDBGateway.desktop
+sed -i "10c Name[es_ES]=Abrir ircDDB" /home/pi/Abrir_ircDDBGateway.desktop
 sed -i "1c D-STAR=OFF" /home/pi/status.ini
 sleep 1
 cd /home/pi
-sudo cp ircDDBGateway /home/pi/Desktop
+sudo cp Abrir_ircDDBGateway.desktop /home/pi/Desktop
 sleep 1
-sudo rm /home/pi/ircDDBGateway
+sudo rm /home/pi/Abrir_ircDDBGateway.desktop
 
 #cierra D-STARRepeater y quita icono verde de "cerrar Cerrar D-STARRepeater"
 cd /home/pi/Desktop
