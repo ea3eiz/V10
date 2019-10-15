121dee1e731c552f8f74e6a8e70fa01ac2fd2231
Manuel Díaz Jiménez <manel@iMac-de-Manuel.local>
2019-10-15 17:03:11 +0200

    Update editar_MMDVMLIBRE.sh

--- editar_MMDVMLIBRE.sh
+++ editar_MMDVMLIBRE.sh
@@ -8,7 +8,7 @@ usuario=$(awk "NR==1" /home/pi/.config/autostart/usuario)
 # path usuario
 SCRIPTS_version=$(awk "NR==1" $usuario/.config/autostart/version)
 
-#Editor MMDVMLIBRE.ini
+#Editor MMDVMLIBRE.ini 
 DIRECTORIO="MMDVMLIBRE.ini"
 DIRECTORIO_copia="MMDVMLIBRE.ini_copia"
 DIRECTORIO_copia2="MMDVMLIBRE.ini_copia2"
@@ -45,10 +45,17 @@ txf=`grep -n "TXFrequency" $usuario/MMDVMHost/MMDVMLIBRE.ini`
 txf1=`expr substr $txf 4 30`
 echo "$txf1"
 
-echo -n "\33[1;36m   4)\33[0m Modificar Location    - \33[1;33m"
-loca=`grep -n "Locatio" $usuario/MMDVMHost/MMDVMLIBRE.ini`
-loca1=`expr substr $loca 4 30`
-echo "$loca1"
+echo -n "${CIAN}   4)${GRIS} Modificar Location    - ${AMARILLO}"
+loc=`grep -n "^Location=" $usuario/MMDVMHost/$DIRECTORIO`
+loc1=`echo "$loc" | tr -d '[[:space:]]'`
+buscar=":"
+largo_linea=`expr index $loc1 $buscar`
+largo_linea=`expr $largo_linea - 1`
+numero_linea=`expr substr $loc1 1 $largo_linea`
+letrac=c
+numero_linea_letrac=$numero_linea$letrac
+contenido_location=$(awk "NR==$numero_linea" $usuario/MMDVMHost/$DIRECTORIO)
+echo "$contenido_location"
 
 echo -n "\33[1;36m   5)\33[0m Modificar URL         - \33[1;33m"
 url=`grep -n "URL" $usuario/MMDVMHost/MMDVMLIBRE.ini`
@@ -270,26 +277,15 @@ done;;
 4) echo ""
 while true
 do
-buscar=":"
-largo=`expr index $loca $buscar`
-echo "Valor de la Ciudad: \33[1;33m${loca#*=}\33[1;37m"
-           	          read -p 'Introduce tu Ciudad ' loc1
-                          letra=c
-                          if [ $largo = 3 ]
-                          then
-                          linea=`expr substr $loca 1 2`
-                          else
-                          linea=`expr substr $loca 1 3`
-                          fi
-                          linea=$linea$letra
+                          echo "Valor de la Ciudad: ${AMARILLO}${contenido_location#*=}\33[1;37m"
+                          read -p 'Introduce tu Ciudad ' loc1
                           actualizar=S 
                           case $actualizar in
-			  [sS]* ) echo ""
-			  loc1=`echo "$loc1" | tr -d '[[:space:]]'`
-              sed -i "$linea Location=$loc1" $usuario/MMDVMHost/MMDVMLIBRE.ini
-			  break;;
-			  [nN]* ) echo ""
-			  break;;
+                          [sS]* ) echo ""
+                          sed -i "$numero_linea_letrac Location=$loc1" $usuario/MMDVMHost/$DIRECTORIO
+                          break;;
+                          [nN]* ) echo ""
+                          break;;
 esac
 done;;
 5) echo ""
