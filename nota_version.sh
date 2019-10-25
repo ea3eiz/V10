3a2d7048aae040ed7cf0aa0d02f12e59656f705f
Manel <ea3eiz@ea3eiz.com>
2019-10-25 12:20:14 +0200

    Update nota_version.sh

--- nota_version.sh
+++ nota_version.sh
@@ -20,7 +20,7 @@ echo "\33[1;32m"
 echo "   Imagen Linux basada en Debian Stretch."
 echo ""
 echo "   Participants in de proyect:"
-echo "${BLANCO}   -EA3EIZ (Programer & Designer) -EA3EG (Innovative $ System Tester)"
+echo "${BLANCO}   -EA3EIZ (Programer & Designer) -EA3EG (Innovative & System Tester)"
 
 echo ""
 echo "\33[1;33m   - Utilizar adecuadamente los puertos de los correspondientes Módems."
