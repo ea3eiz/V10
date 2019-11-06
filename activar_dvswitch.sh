306eda70553b9befc95ae2d1e3296b8eb7f12454
Manel <ea3eiz@ea3eiz.com>
2019-11-06 23:05:28 +0100

    1

--- activar_dvswitch.sh
+++
@@ -1,6 +0,0 @@
-﻿#!/bin/bash
-sudo systemctl restart analog_bridge.service
-sudo systemctl restart ircddbgateway.service
-sudo systemctl restart md380-emu.service
-sudo systemctl restart mmdvm_bridge.service
-sudo systemctl restart nxdngateway.service
--- stop_dvswitch.sh
+++ stop_dvswitch.sh
@@ -12,7 +12,7 @@ sleep 2
 sudo rm /home/pi/Activar_dvswitch.desktop
 
 
-systemctl stop analog_bridge.service
+sudo systemctl stop analog_bridge.service
 sudo systemctl stop ircddbgateway.service
 sudo systemctl stop md380-emu.service
 sudo systemctl stop mmdvm_bridge.service
