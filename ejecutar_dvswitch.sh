d46f066d0efc862b67c7248ae57b13a2e71e2460
Manel <ea3eiz@ea3eiz.com>
2019-11-06 20:37:15 +0100

    Update ejecutar_dvswitch.sh

--- ejecutar_dvswitch.sh
+++ ejecutar_dvswitch.sh
@@ -4,7 +4,7 @@ cd /home/pi/Desktop
 sudo cp Activar_dvswitch.desktop /home/pi
 sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version;lxterminal --geometry=72x15 -e sudo sh stop_dvswitch.sh'" /home/pi/Activar_dvswitch.desktop
 sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICONO_AMBE_SERVER_ON.png" /home/pi/Activar_dvswitch.desktop
-sed -i "10c Name[es_ES]=Cerrar AMBE SERVER" /home/pi/Activar_dvswitch.desktop
+sed -i "10c Name[es_ES]=Parar Dvswitch" /home/pi/Activar_dvswitch.desktop
 
 cd /home/pi
 sudo cp Activar_dvswitch.desktop /home/pi/Desktop
