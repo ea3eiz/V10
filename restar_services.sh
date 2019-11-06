ec1c4f36fb5e2e96fed7dcf0c0a2e065b07f75d9
Manel <ea3eiz@ea3eiz.com>
2019-11-06 20:10:56 +0100

    Create restar_services.sh

---
+++ restar_services.sh
@@ -0,0 +1,6 @@
+﻿#!/bin/bash
+sudo systemctl stop analog_bridge.service
+sudo systemctl stop ircddbgateway.service
+sudo systemctl stop md380-emu.service
+sudo systemctl stop mmdvm_bridge.service
+sudo systemctl stop nxdngateway.service
