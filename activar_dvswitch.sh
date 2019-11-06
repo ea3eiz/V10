bb974f9cc9c18e1cebb18f897684ee24134c5940
Manel <ea3eiz@ea3eiz.com>
2019-11-06 20:25:10 +0100

    Create activar_dvswitch.sh

---
+++ activar_dvswitch.sh
@@ -0,0 +1,6 @@
+﻿#!/bin/bash
+sudo systemctl start analog_bridge.service
+sudo systemctl start ircddbgateway.service
+sudo systemctl start md380-emu.service
+sudo systemctl start mmdvm_bridge.service
+sudo systemctl start nxdngateway.service
