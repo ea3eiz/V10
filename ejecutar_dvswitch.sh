dd27ba7d57eb725e4b89ee23991ed6a6dc144ff8
Manel <ea3eiz@ea3eiz.com>
2019-11-06 22:57:44 +0100

    1

--- ejecutar_dvswitch.sh
+++ ejecutar_dvswitch.sh
@@ -2,7 +2,7 @@
 SCRIPTS_version=$(awk "NR==1" /home/pi/.config/autostart/version)
 cd /home/pi/Desktop
 sudo cp Activar_dvswitch.desktop /home/pi
-sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version;lxterminal --geometry=72x15 -e sudo sh stop_dvswitch.sh'" /home/pi/Activar_dvswitch.desktop
+sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version; sudo sh stop_dvswitch.sh'" /home/pi/Activar_dvswitch.desktop
 sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICONO_AMBE_SERVER_ON.png" /home/pi/Activar_dvswitch.desktop
 sed -i "10c Name[es_ES]=Parar Dvswitch" /home/pi/Activar_dvswitch.desktop
 
--- stop_dvswitch.sh
+++ stop_dvswitch.sh
@@ -9,7 +9,7 @@ sudo systemctl stop nxdngateway.service
 SCRIPTS_version=$(awk "NR==1" /home/pi/.config/autostart/version)
 cd /home/pi/Desktop
 sudo cp Activar_dvswitch.desktop /home/pi
-sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version;lxterminal --geometry=72x15 -e sudo sh ejecutar_dvswitch.sh'" /home/pi/Activar_dvswitch.desktop
+sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version; sudo sh ejecutar_dvswitch.sh'" /home/pi/Activar_dvswitch.desktop
 sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICONO_AMBE_SERVER_OFF.png" /home/pi/Activar_dvswitch.desktop
 sed -i "10c Name[es_ES]=Activar Dvswitch" /home/pi/Activar_dvswitch.desktop
 
