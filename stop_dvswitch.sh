3bd69f34fe24a19bf3dd38b072aca35bf179abc7
Manel <ea3eiz@ea3eiz.com>
2019-11-06 23:00:50 +0100

    Update stop_dvswitch.sh

--- stop_dvswitch.sh
+++ stop_dvswitch.sh
@@ -1,11 +1,4 @@
 ﻿#!/bin/bash
-sudo systemctl stop analog_bridge.service
-sudo systemctl stop ircddbgateway.service
-sudo systemctl stop md380-emu.service
-sudo systemctl stop mmdvm_bridge.service
-sudo systemctl stop nxdngateway.service
-
-
 SCRIPTS_version=$(awk "NR==1" /home/pi/.config/autostart/version)
 cd /home/pi/Desktop
 sudo cp Activar_dvswitch.desktop /home/pi
@@ -18,5 +11,9 @@ sudo cp Activar_dvswitch.desktop /home/pi/Desktop
 sleep 2
 sudo rm /home/pi/Activar_dvswitch.desktop
 
-cd /home/pi/$SCRIPTS_version
-sudo sh stop_dvswitch.sh
+
+systemctl stop analog_bridge.service
+sudo systemctl stop ircddbgateway.service
+sudo systemctl stop md380-emu.service
+sudo systemctl stop mmdvm_bridge.service
+sudo systemctl stop nxdngateway.service
