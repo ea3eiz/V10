61c2bb03a6aa04e663b4bf88ed4363babfd7a3ec
Manel <ea3eiz@ea3eiz.com>
2019-11-07 12:28:13 +0100

    Update ejecutar_dvswitch.sh

--- ejecutar_dvswitch.sh
+++ ejecutar_dvswitch.sh
@@ -1,5 +1,28 @@
 #!/bin/bash
+
 SCRIPTS_version=$(awk "NR==1" /home/pi/.config/autostart/version)
+
+cd /home/pi/$SCRIPTS_version
+sudo sh cerrar_ambe_server.sh
+sudo sh cerrar_bluedv.sh
+sudo sh cerrar_bm.sh
+sudo sh cerrar_d-star.sh
+sudo sh cerrar_DMR2NXDN.sh
+sudo sh cerrar_DMR2YSF.sh
+sudo sh cerrar_DMRPLUS.sh
+sudo sh cerrar_dstarrepeater_30.sh
+sudo sh cerrar_dv4mini.sh
+sudo sh cerrar_dvrptr.sh
+sudo sh cerrar_LIBRE.sh
+sudo sh cerrar_NXDN.sh
+sudo sh cerrar_radio.sh
+sudo sh cerrar_solodstar.sh
+sudo sh cerrar_solofusion.sh
+sudo sh cerrar_ysf.sh
+sudo sh cerrar_YSF2DMR.sh
+
+sleep 2
+
 cd /home/pi/Desktop
 sudo cp Activar_dvswitch.desktop /home/pi
 sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version; sudo sh stop_dvswitch.sh'" /home/pi/Activar_dvswitch.desktop
