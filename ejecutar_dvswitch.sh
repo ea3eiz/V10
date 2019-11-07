5f71c73321d0d6e0fb305bffc4471f46228c0f33
Manel <ea3eiz@ea3eiz.com>
2019-11-07 12:42:16 +0100

    Update ejecutar_dvswitch.sh

--- ejecutar_dvswitch.sh
+++ ejecutar_dvswitch.sh
@@ -7,16 +7,16 @@ echo "CERRANDO TODOS LOS SISTEMAS"
 
 
 cd /home/pi/$SCRIPTS_version
-sudo sh cerrar_ambe_server.sh
-sudo sh cerrar_bluedv.sh
+#sudo sh cerrar_ambe_server.sh
+#sudo sh cerrar_bluedv.sh
 sudo sh cerrar_bm.sh
 sudo sh cerrar_d-star.sh
 sudo sh cerrar_DMR2NXDN.sh
 sudo sh cerrar_DMR2YSF.sh
 sudo sh cerrar_DMRPLUS.sh
 sudo sh cerrar_dstarrepeater_30.sh
-sudo sh cerrar_dv4mini.sh
-sudo sh cerrar_dvrptr.sh
+#sudo sh cerrar_dv4mini.sh
+#sudo sh cerrar_dvrptr.sh
 sudo sh cerrar_LIBRE.sh
 sudo sh cerrar_NXDN.sh
 sudo sh cerrar_radio.sh
