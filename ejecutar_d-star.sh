b2a270e78705458b9ce804a3306f0e0f8c6525fa
Manel <ea3eiz@ea3eiz.com>
2019-11-08 21:00:47 +0100

    Update ejecutar_d-star.sh

--- ejecutar_d-star.sh
+++ ejecutar_d-star.sh
@@ -2,8 +2,8 @@
 
 modo=$(awk "NR==18" /home/pi/status.ini)
 if [ "$modo" = 'DVSWITCH=ON' ];then
-cd /home/pi/V10
-sudo sh ejecutar_bm.sh
+sudo sh /home/pi/V10/ejecutar_bm.sh
+
 else
 SCRIPTS_version=$(awk "NR==1" /home/pi/.config/autostart/version)
 cd /home/pi/Desktop
