41240528e5bef693dda68b551a5453df6a6d6767
Manel <ea3eiz@ea3eiz.com>
2019-11-15 00:33:21 +0100

    1

--- ejecutar_bluedv.sh
+++ ejecutar_bluedv.sh
@@ -1,32 +1,5 @@
 #!/bin/bash
 
-
-clear
-ROJO="\033[1;31m"
-VERDE="\033[1;32m"
-BLANCO="\033[1;37m"
-AMARILLO="\033[1;33m"
-CIAN="\033[1;36m"
-GRIS="\033[0m"
-echo "${VERDE}"
-echo "   *************************************************************************"
-echo "   *                                                                       *"
-echo "   *              El BlueDV puede tardar 1 minuto en abrir                 *"
-echo "   *           dependiendo de la velocidad de su raspberry pi              *"
-echo "   *             Por favor sea paciente y espere que se abra               *"
-echo "   *                                                                       *"
-echo "   *************************************************************************"
-echo "${BLANCO}"
-echo "   *************************************************************************"
-echo "   *                                                                       *"
-echo "   *       Esta ventana se cerrará sola cuando arranque el BlueDV          *"
-echo "   *                                                                       *"
-echo "   * ${ROJO}                  IMPORTANTE!!! NO LA CIERRE ANTES                    ${BLANCO}*"
-echo "   * ${BLANCO}                                                                      *"
-echo "   *************************************************************************"
-echo ""
-
-
 SCRIPTS_version=$(awk "NR==1" /home/pi/.config/autostart/version)
 cd /home/pi/Desktop
 sudo cp Abrir_BlueDV.desktop /home/pi
@@ -42,4 +15,3 @@ sudo rm /home/pi/Abrir_BlueDV.desktop
 
 cd /home/pi/bluedv/
 mono BlueDV.exe
-exit
