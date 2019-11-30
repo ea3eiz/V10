a88a0daf79db8ed73bff254f1f2598d57497ecdc
Manel <ea3eiz@ea3eiz.com>
2019-11-30 18:52:59 +0100

    Update crear_zip.sh

--- crear_zip.sh
+++ crear_zip.sh
@@ -72,7 +72,7 @@ indicativo=$(awk "NR==2" /opt/MMDVM_Bridge/MMDVM_Bridge.ini)
 indicativo=`expr substr $indicativo 10 6`
 address_especial=$(awk "NR==70" /opt/MMDVM_Bridge/especial.ini)
 id=$(awk "NR==38" /opt/Analog_Bridge/Analog_Bridge.ini)
-#id=`expr substr $id 16 5`
+id=`expr substr $id 16 7`
 
 sudo sed -i "1c $indicativo" /home/pi/Downloads/datos_dvswitch
 sudo sed -i "2c $address_especial" /home/pi/Downloads/datos_dvswitch
