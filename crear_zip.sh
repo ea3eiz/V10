212264178eda82ede70ac86dec64d4c389a224e7
Manel <ea3eiz@ea3eiz.com>
2019-11-30 22:25:30 +0100

    1

--- crear_zip.sh
+++ crear_zip.sh
@@ -105,19 +105,19 @@ sala_fcs=`expr substr $sala_fcs 6 20`
 
 sala_nxdn=$(awk "NR==10" /opt/NXDNClients/NXDNGateway/private/NXDNHosts.txt)
 
-sudo sed -i "1c $indicativo" /home/pi/Downloads/datos_dvswitch
-sudo sed -i "2c $address_especial" /home/pi/Downloads/datos_dvswitch
-sudo sed -i "3c $id" /home/pi/Downloads/datos_dvswitch
-sudo sed -i "4c $id2" /home/pi/Downloads/datos_dvswitch
-sudo sed -i "5c $Latitude" /home/pi/Downloads/datos_dvswitch
-sudo sed -i "6c $Longitude" /home/pi/Downloads/datos_dvswitch
-sudo sed -i "7c $port" /home/pi/Downloads/datos_dvswitch
-sudo sed -i "8c $location" /home/pi/Downloads/datos_dvswitch
-sudo sed -i "9c $url" /home/pi/Downloads/datos_dvswitch
-sudo sed -i "10c $password_especial" /home/pi/Downloads/datos_dvswitch
-sudo sed -i "11c $port_especial" /home/pi/Downloads/datos_dvswitch
-sudo sed -i "12c $sala_fcs" /home/pi/Downloads/datos_dvswitch
-sudo sed -i "13c $sala_nxdn" /home/pi/Downloads/datos_dvswitch
+sudo sed -i "1c $indicativo" /home/pi/datos_dvswitch
+sudo sed -i "2c $address_especial" /home/pi/datos_dvswitch
+sudo sed -i "3c $id" /home/pi/datos_dvswitch
+sudo sed -i "4c $id2" /home/pi/datos_dvswitch
+sudo sed -i "5c $Latitude" /home/pi/datos_dvswitch
+sudo sed -i "6c $Longitude" /home/pi/datos_dvswitch
+sudo sed -i "7c $port" /home/pi/datos_dvswitch
+sudo sed -i "8c $location" /home/pi/datos_dvswitch
+sudo sed -i "9c $url" /home/pi/datos_dvswitch
+sudo sed -i "10c $password_especial" /home/pi/datos_dvswitch
+sudo sed -i "11c $port_especial" /home/pi/datos_dvswitch
+sudo sed -i "12c $sala_fcs" /home/pi/datos_dvswitch
+sudo sed -i "13c $sala_nxdn" /home/pi/datos_dvswitch
 
 cd /home/pi/
 tar -zcvf copia.tar.gz Downloads
