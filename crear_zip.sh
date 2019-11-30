c453f1095ef4962566229822f5552f2eb0435f2b
Manel <ea3eiz@ea3eiz.com>
2019-11-30 17:36:50 +0100

    Update crear_zip.sh

--- crear_zip.sh
+++ crear_zip.sh
@@ -71,15 +71,21 @@ indicativo=$(awk "NR==2" /opt/MMDVM_Bridge/MMDVM_Bridge.ini)
 indicativo=`expr substr $indicativo 10 6`
 echo "$indicativo"
 read a
-sudo sed -i '1c Callsign=$indicativo' /home/pi/Downloads/datos_dvswitch);
-sudo sed -i '2c Callsign=$indicativo' /home/pi/Downloads/datos_dvswitch);
-sudo sed -i '3c Callsign=$indicativo' /home/pi/Downloads/datos_dvswitch);
-sudo sed -i '4c Callsign=$indicativo' /home/pi/Downloads/datos_dvswitch);
-sudo sed -i '5c Callsign=$indicativo' /home/pi/Downloads/datos_dvswitch);
-sudo sed -i '6c gatewayCallsign=$indicativo' /home/pi/Downloads/datos_dvswitch);
-sudo sed -i '7c ircddbUsername=$indicativo' /home/pi/Downloads/datos_dvswitch);
-sudo sed -i '8c dplusLogin=$indicativo' /home/pi/Downloads/datos_dvswitch);
-sudo sed -i '9c Callsign=$indicativo' /home/pi/Downloads/datos_dvswitch);
+
+
+
+
+
+
+sudo sed -i "1c Callsign=$indicativo" /home/pi/Downloads/datos_dvswitch
+sudo sed -i '2c Callsign=$indicativo' /home/pi/Downloads/datos_dvswitch
+sudo sed -i '3c Callsign=$indicativo' /home/pi/Downloads/datos_dvswitch
+sudo sed -i '4c Callsign=$indicativo' /home/pi/Downloads/datos_dvswitch
+sudo sed -i '5c Callsign=$indicativo' /home/pi/Downloads/datos_dvswitch
+sudo sed -i '6c gatewayCallsign=$indicativo' /home/pi/Downloads/datos_dvswitch
+sudo sed -i '7c ircddbUsername=$indicativo' /home/pi/Downloads/datos_dvswitch
+sudo sed -i '8c dplusLogin=$indicativo' /home/pi/Downloads/datos_dvswitch
+sudo sed -i '9c Callsign=$indicativo' /home/pi/Downloads/datos_dvswitch
 
 cd /home/pi/
 tar -zcvf copia.tar.gz Downloads
