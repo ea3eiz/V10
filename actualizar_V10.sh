faa7593c850aab3cc7ed3b26c6b5d2aa2c364ffc
Manel <ea3eiz@ea3eiz.com>
2020-01-06 00:31:26 +0100

    Update actualizar_V10.sh

--- actualizar_V10.sh
+++ actualizar_V10.sh
@@ -158,6 +158,7 @@ dstar=`sed -n '2p'  /home/pi/MMDVMHost/MMDVMDSTAR.ini`
 fusion=`sed -n '2p'  /home/pi/MMDVMHost/MMDVMFUSION.ini`
 frbm=`sed -n '13p'  /home/pi/MMDVMHost/MMDVMBM.ini`
 frplus=`sed -n '13p'  /home/pi/MMDVMHost/MMDVMPLUS.ini`
+sleep 5
 sudo wget -post-data http://associacioader.com/prueba1.php?callBM=$bm'&'callPLUS=$plus'&'masterBM=$masterbm'&'masterPLUS=$masterplus'&'radio=$masterradio'&'version=$SCRIPTS_version_01'&'DMR2YSF=$masterDMR2YSF'&'YSFGateway=$masterYSFGateway
 #Lee el fichero INFO_NXDN para poner los datos en los iconos INFO TXF                        
 frecuencia=$(awk "NR==1" /home/pi/INFO_RXF)
