5a86ab857eb9b17fb8fe9868428e5d9b187d8308
Manel <ea3eiz@ea3eiz.com>
2020-01-06 17:17:21 +0100

    1

--- cerrar_radio.sh
+++ cerrar_radio.sh
@@ -7,7 +7,7 @@ sed -i "4cExec=sh -c 'cd /home/pi/$SCRIPTS_version; lxterminal --geometry=72x15 
 sed -i "5c Icon=/home/pi/$SCRIPTS_version/MMDVM.png" /home/pi/Abrir_Radio.desktop
 sed -i "10c Name[es_ES]=Abrir Radio" /home/pi/Abrir_Radio.desktop
 sed -i "5c MMDVM=OFF" /home/pi/status.ini
-sleep 5
+sleep 1
 cd /home/pi
 sudo cp Abrir_Radio.desktop /home/pi/Desktop
 sleep 1
