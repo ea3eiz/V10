d548ed2a5c84c9cf4fba40427bf8ed0432e1a58a
Manel <ea3eiz@ea3eiz.com>
2020-01-06 17:25:42 +0100

    Update ejecutar_radio.sh

--- ejecutar_radio.sh
+++ ejecutar_radio.sh
@@ -36,17 +36,14 @@ sed -i "4c $frecuencia" /home/pi/INFO_RXF
 
 SCRIPTS_version=$(awk "NR==1" /home/pi/.config/autostart/version)
 cd /home/pi/Desktop
-sudo cp /home/pi/Desktop/Abrir_Radio.desktop /home/pi
-sleep 5
+sudo cp Abrir_Radio.desktop /home/pi
 sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version;lxterminal --geometry=72x15 -e sudo sh cerrar_radio.sh'" /home/pi/Abrir_Radio.desktop
 sed -i "5c Icon=/home/pi/$SCRIPTS_version/MMDVM_ON.png" /home/pi/Abrir_Radio.desktop
 sed -i "10c Name[es_ES]=Cerrar Radio" /home/pi/Abrir_Radio.desktop
 sed -i "5c MMDVM=ON" /home/pi/status.ini
-sleep 5
 cd /home/pi
-sleep 5
 sudo cp Abrir_Radio.desktop /home/pi/Desktop
-
+sleep 1
 sudo rm /home/pi/Abrir_Radio.desktop
 
 cd /home/pi/MMDVMHost
