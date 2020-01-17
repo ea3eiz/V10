29f22773f75678cad3222338db78cb986b850c20
Manel <ea3eiz@ea3eiz.com>
2020-01-17 18:04:13 +0100

    1

--- ejecutar_dstarrepeater.sh
+++ ejecutar_dstarrepeater.sh
@@ -22,7 +22,7 @@ else
 SCRIPTS_version=$(awk "NR==1" /home/pi/.config/autostart/version)
 cd /home/pi/Desktop
 sudo cp Abrir_ircDDBGateway.desktop /home/pi
-sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version; sudo sh cerrar_d-star_30.sh'" /home/pi/Abrir_ircDDBGateway.desktop
+sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version; sudo sh cerrar_d-star.sh'" /home/pi/Abrir_ircDDBGateway.desktop
 sed -i "5c Icon=/home/pi/$SCRIPTS_version/DSTAR_ON.png" /home/pi/Abrir_ircDDBGateway.desktop
 sed -i "10c Name[es_ES]=Cerrar ircDDB" /home/pi/Abrir_ircDDBGateway.desktop
 sed -i "1c D-STAR=ON" /home/pi/status.ini
@@ -32,15 +32,15 @@ sleep 1
 sudo rm /home/pi/Abrir_ircDDBGateway.desktop
 
 cd /home/pi/Desktop
-sudo cp Abrir_D-STARRepeater /home/pi
-sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version; sudo sh cerrar_dstarrepeater_30.sh'" /home/pi/Abrir_D-STARRepeater
-sed -i "5c Icon=/home/pi/$SCRIPTS_version/REPEATER_ON.png" /home/pi/Abrir_D-STARRepeater
-sed -i "10c Name[es_ES]=Cerrar D-STAR Repeater" /home/pi/Abrir_D-STARRepeater
+sudo cp Abrir_D-STARRepeater.desktop /home/pi
+sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version; sudo sh cerrar_dstarrepeater.sh'" /home/pi/Abrir_D-STARRepeater.desktop
+sed -i "5c Icon=/home/pi/$SCRIPTS_version/REPEATER_ON.png" /home/pi/Abrir_D-STARRepeater.desktop
+sed -i "10c Name[es_ES]=Cerrar D-STAR Repeater" /home/pi/Abrir_D-STARRepeater.desktop
 sed -i "9c dstarrepeater=ON" /home/pi/status.ini
 cd /home/pi
-sudo cp Abrir_D-STARRepeater /home/pi/Desktop
+sudo cp Abrir_D-STARRepeater.desktop /home/pi/Desktop
 sleep 1
-sudo rm /home/pi/Abrir_D-STARRepeater
+sudo rm /home/pi/Abrir_D-STARRepeater.desktop
 
 #sudo ircddbgateway -gui  & dstarrepeater & ircddbgateway -gui
 sudo ircddbgateway -gui  & 
