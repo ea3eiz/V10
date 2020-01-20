e1d8cb55dc152787b0b57d33054fd618f6cb352a
Manel <ea3eiz@ea3eiz.com>
2020-01-20 01:08:40 +0100

    Update ejecutar_bm.sh

--- ejecutar_bm.sh
+++ ejecutar_bm.sh
@@ -40,9 +40,7 @@ cd /home/pi/Desktop
 sudo cp Abrir_MMDVMBM.desktop /home/pi
 sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version; lxterminal --geometry=72x15 -e sudo sh cerrar_bm.sh'" /home/pi/Abrir_MMDVMBM.desktop
 sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICONO_BM_ON.png" /home/pi/Abrir_MMDVMBM.desktop
-sed -i "10c Name[es_ES]=Cerrar_BM" /home/pi/Abrir_MMDVMBM.desktop
-sed -i "12c Name=Cerrar_BM" /home/pi/Abrir_MMDVMBM.desktop
-
+sed -i "10c Name[es_ES]=Cerrar BM" /home/pi/Abrir_MMDVMBM.desktop
 sed -i "7c MMDVMBM=ON" /home/pi/status.ini
 cd /home/pi
 sudo cp Abrir_MMDVMBM.desktop /home/pi/Desktop
