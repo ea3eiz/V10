ae98f2f6aa4ec878fc2aa53175ceb12cd4807ba9
Manel <ea3eiz@ea3eiz.com>
2020-02-06 12:48:06 +0100

    Update ejecutar_solodstar.sh

--- ejecutar_solodstar.sh
+++ ejecutar_solodstar.sh
@@ -36,15 +36,15 @@ sed -i "5c $frecuencia" /home/pi/INFO_RXF
 
 SCRIPTS_version=$(awk "NR==1" /home/pi/.config/autostart/version)
 cd /home/pi/Desktop
-sudo cp AbrirsoloDstar.desktop /home/pi
-sed -i "6c Exec=sudo sh cerrar_solodstar.sh" /home/pi/AbrirsoloDstar.desktop
-sed -i "7c Icon=/home/pi/$SCRIPTS_version/SOLO_D-STAR_ON.png" /home/pi/AbrirsoloDstar.desktop
-sed -i "11c Name[es_ES]=Cerrar solo D-STAR" /home/pi/AbrirsoloDstar.desktop
+sudo cp Abrir_solodstar.desktop /home/pi
+sed -i "6c Exec=sudo sh cerrar_solodstar.sh" /home/pi/Abrir_solodstar.desktop
+sed -i "7c Icon=/home/pi/$SCRIPTS_version/SOLO_D-STAR_ON.png" /home/pi/Abrir_solodstar.desktop
+sed -i "11c Name[es_ES]=Cerrar solo D-STAR" /home/pi/Abrir_solodstar.desktop
 sed -i "13c SOLODSTAR=ON" /home/pi/status.ini
 cd /home/pi
-sudo cp AbrirsoloDstar.desktop /home/pi/Desktop
+sudo cp Abrir_solodstar.desktop /home/pi/Desktop
 sleep 1
-sudo rm /home/pi/AbrirsoloDstar.desktop
+sudo rm /home/pi/Abrir_solodstar.desktop
 
 cd /home/pi/MMDVMHost
 echo "\33[1;32m"
