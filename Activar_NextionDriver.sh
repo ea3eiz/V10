3855b6ea64aeda37f870c05890112210f02e4f7a
Manel <ea3eiz@ea3eiz.com>
2020-02-11 22:18:20 +0100

    1

--- Activar_NextionDriver.sh
+++ Activar_NextionDriver.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 sudo sed -i "4c Exec=sh -c 'cd /home/pi/NextionDriver/;sudo ./NextionDriver -c /home/pi/MMDVMHost/MMDVM.ini'" /home/pi/.config/autostart/nextiondriver.desktop
-echo "si activas el Nextion driver"
+echo "************************************************"
+echo "  Si activas NextionDriver no funcionará "
+echo "  La funciona la china pinchada en la raspberry"
+echo "************************************************"
+
 read a
