7b5b1f46417ebb67dca3bf46c634dc9f6eb78d72
Manel <ea3eiz@ea3eiz.com>
2020-02-11 22:22:56 +0100

    Update Activar_NextionDriver.sh

--- Activar_NextionDriver.sh
+++ Activar_NextionDriver.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
+#Colores
+ROJO="\033[1;31m"
+VERDE="\033[1;32m"
+BLANCO="\033[1;37m"
+AMARILLO="\033[1;33m"
+CIAN="\033[1;36m"
+GRIS="\033[0m"
 sudo sed -i "4c Exec=sh -c 'cd /home/pi/NextionDriver/;sudo ./NextionDriver -c /home/pi/MMDVMHost/MMDVM.ini'" /home/pi/.config/autostart/nextiondriver.desktop
-echo "************************************************"
+echo "${AMARILLO}"
+echo "********************************************************************"
 echo "  Si activas NextionDriver no funcionará "
 echo "  La funciona la china pinchada en la raspberry"
 echo "************************************************"
