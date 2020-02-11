2ce1d35b0770ae479737d369e0bf616d4d7bc9ce
Manel <ea3eiz@ea3eiz.com>
2020-02-11 21:32:02 +0100

    Create Activar_NextionDriver.sh

---
+++ Activar_NextionDriver.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+SCRIPTS_version=$(awk "NR==1" /home/pi/.config/autostart/version)
+sed -i "4c Exec=sh -c 'cd /home/pi/NextionDriver/;sudo ./NextionDriver -c /home/pi/MMDVMHost/MMDVM.ini
+'" /home/pi/Abrir_dv4mini.desktop
