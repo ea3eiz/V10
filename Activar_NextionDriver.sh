4c3b3ba895e5e30f0678972983f95cfda8d5235f
Manel <ea3eiz@ea3eiz.com>
2020-02-12 22:33:10 +0100

    1

--- Activar_NextionDriver.sh
+++ Activar_NextionDriver.sh
@@ -43,7 +43,7 @@ sudo rm Activar_NextionDriver.desktop
 
 sudo sed -i "4c Exec=sh -c 'cd /home/pi/NextionDriver/;sudo ./NextionDriver -c /home/pi/MMDVMHost/MMDVM.ini'" /home/pi/.config/autostart/nextiondriver.desktop
 
-sudo reboot
+#sudo reboot
 
 break;;
 [nN]*) 
