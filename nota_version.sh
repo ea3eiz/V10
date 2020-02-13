2d6a31643b26528365601581594b6443ce0a752e
Manel <ea3eiz@ea3eiz.com>
2020-02-13 22:43:50 +0100

    Update nota_version.sh

--- nota_version.sh
+++ nota_version.sh
@@ -15,7 +15,11 @@ echo -n "${VERDE}"
 echo "   ************************************************************************************"
 echo "\33[1;36m   Gracias por descargar la imagen de ADER V.02.07.$NUMERO_VERSION"
 echo ""
-cat /etc/os-release
+
+info=$(cat /etc/os-release)
+info=`expr substr $info 1 45`
+echo "$info"
+
 echo "\33[1;32m   Imagen multi plataforma para \33[1;33mRADIO AFICIONADOS."
 echo "\33[1;32m"
 echo "   Imagen Linux basada en Debian Stretch."
