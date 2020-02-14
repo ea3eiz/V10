224b917ac280c6aebc78a7d3cd3b4da631223992
Manel <ea3eiz@ea3eiz.com>
2020-02-14 15:18:15 +0100

    Update actualizar.sh

--- actualizar.sh
+++ actualizar.sh
@@ -236,7 +236,7 @@ rm /home/pi/RXF_NXDN.desktop
 
 status_dvswitch=$(awk "NR==18" /home/pi/status.ini)
 status_dvswitch=`expr substr $status_dvswitch 10 3`
-if [ $status_dvswitch =  OFF ]; then
+if [ $status_dvswitch = OFF ]; then
 systemctl stop ircddbgateway.service
 sudo killall ircddbgatewayd
 else
