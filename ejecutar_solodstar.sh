00e29f91450d817883114b97e7a56d9040dcaa75
Manel <ea3eiz@ea3eiz.com>
2020-03-19 23:35:12 +0100

    Update ejecutar_solodstar.sh

--- ejecutar_solodstar.sh
+++ ejecutar_solodstar.sh
@@ -46,6 +46,27 @@ sudo cp Abrir_solodstar.desktop /home/pi/Desktop
 sleep 1
 sudo rm /home/pi/Abrir_solodstar.desktop
 
+
+
+
+
+
+cd /home/pi/Desktop
+sudo cp Abrir_ircDDBGateway.desktop /home/pi
+sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version; sudo sh cerrar_d-star.sh'" /home/pi/Abrir_ircDDBGateway.desktop
+sed -i "5c Icon=/home/pi/$SCRIPTS_version/DSTAR_ON.png" /home/pi/Abrir_ircDDBGateway.desktop
+sed -i "10c Name[es_ES]=Cerrar ircDDB" /home/pi/Abrir_ircDDBGateway.desktop
+sed -i "1c D-STAR=ON" /home/pi/status.ini
+cd /home/pi
+sudo cp Abrir_ircDDBGateway.desktop /home/pi/Desktop
+sleep 1
+sudo rm /home/pi/Abrir_ircDDBGateway.desktop
+
+
+
+
+
+
 cd /home/pi/MMDVMHost
 echo "\33[1;32m"
 clear
