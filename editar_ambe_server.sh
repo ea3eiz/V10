c3bbe5a76345cb8ab557e76c4758a97d47070f94
Manel <ea3eiz@ea3eiz.com>
2020-06-14 17:17:27 +0200

    1

--- editar_ambe_server.sh
+++ editar_ambe_server.sh
@@ -15,7 +15,7 @@ BLANCO="\033[1;37m"
 AMARILLO="\033[1;33m"
 echo "${VERDE}"
 echo "   ************************************************************************"
-echo "   *        Script para Editor AMBE 17:15 SERVER\33[1;33m       ${ROJO} by EA3EIZ          \33[1;32m     *"
+echo "   *        Script para Editor AMBE 17:20 SERVER\33[1;33m       ${ROJO} by EA3EIZ          \33[1;32m     *"
 echo "   ************************************************************************"
 echo -n "\33[1;36m   1)\33[0m Modificar puerto ROUTER  - \33[1;33m"
 var1= sed -n '1p'  $usuario/ambe_server.ini
